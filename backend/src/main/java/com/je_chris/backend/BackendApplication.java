05bfacd82db75139ff860963742d36ac6a25a5d9
jeason <jeason369chris@163.com>
2021-01-31 14:15:30 +0800

    This is the model of JEBlog, and I'll try to finish and optimize it as soon as possible, hurry upgit status

---
+++ backend/src/main/java/com/je_chris/backend/BackendApplication.java
@@ -0,0 +1,15 @@
+package com.je_chris.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
